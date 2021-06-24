8d636b7e38c3d0a96d2f2be6420331ba853d4189
yang <yyang010@qq.com>
2021-06-24 21:50:44 +0800

    Delete pre_genome.sh

--- pre_genome.sh
+++
@@ -1,4 +0,0 @@
-genomedir=`ls -d /public/home/linHai/project/20200403purge_haplotigs_env_test_lixiaoxia/04ragoo/04fei/CW* \
-/public/home/linHai/project/20200403purge_haplotigs_env_test_lixiaoxia/04ragoo/04fei/NH*`
-
-sampleID=`basename $genomedir`
